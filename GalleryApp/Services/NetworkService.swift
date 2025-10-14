68d18d1072d7a7aeb9362a1cd71cc07a2bfb6dd1
SuperSweetDainty <danya.laputin@mail.ru>
2025-10-14 23:10:49 +0300

    Убран access key

--- GalleryApp/Services/NetworkService.swift
+++ GalleryApp/Services/NetworkService.swift
@@ -6,7 +6,7 @@ protocol NetworkServiceProtocol {
 
 class NetworkService: NetworkServiceProtocol {
     private let baseURL = "https://api.unsplash.com"
-    private let accessKey = "elw8fVRTyxrax_u8IS7Qu_MDuicPoA88mOA0SqV_AlM"
+    private let accessKey = "YOUR_UNSPLASH_ACCESS_KEY"
     
     private lazy var session: URLSession = {
         let config = URLSessionConfiguration.default
